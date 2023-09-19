48386750fc2eaaaa9e74fda57c355dde1d8070b2
cheej <cheej@DESKTOP-PLA7F02>
2023-09-19 16:48:49 +0800

    Initial commit

--- playground/src/main/java/com/packground/config/WebConfig.java
+++ playground/src/main/java/com/packground/config/WebConfig.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "com.playground.controller")
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan(basePackages = "com.playground.controller")
 public class WebConfig {
 
 }
--- playground/src/main/java/com/playground/AppInitializer.java
+++ playground/src/main/java/com/playground/AppInitializer.java
@@ -15,13 +15,13 @@ public class AppInitializer implements WebApplicationInitializer{
 	@Override
 	public void onStartup(ServletContext container) throws ServletException {
 		// TODO Auto-generated method stub
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.scan("com.playground");
-        container.addListener(new ContextLoaderListener(context));
-
-        ServletRegistration.Dynamic dispatcher = container.addServlet("mvc", (Servlet) new DispatcherServlet(context));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");   
+//		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+//        context.scan("com.playground");
+//        container.addListener(new ContextLoaderListener(context));
+//
+//        ServletRegistration.Dynamic dispatcher = container.addServlet("mvc", (Servlet) new DispatcherServlet(context));
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/");   
 	}
 
 }
